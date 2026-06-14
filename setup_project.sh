04e2f9b68b76926f48c76500d18c1281b2f239cc
humwamikaz-ops <h.umwamikaz@alustudent.com>
2026-06-14 11:24:40 -0700

    dynamic configuration

--- setup_project.sh
+++ setup_project.sh
@@ -6,9 +6,19 @@ mkdir -p attendance_tracker_$input
 touch attendance_tracker_$input/attendance_checker.py
 mkdir -p attendance_tracker_$input/Helpers
 touch attendance_tracker_$input/Helpers/assets.csv
-touch attendance_tracker_$input/Helpers/config.json
+cat > attendance_tracker_$input/Helpers/config.json << EOF
+{
+    "warning": 75,
+    "failure": 50
+}
+EOF
 mkdir -p attendance_tracker_$input/reports
 touch attendance_tracker_$input/reports/reports.log
-
-
+read -p "Do you want to make updates to the thresholds? y/n " ip
+if [ "$ip" = "y" ]; then
+	read -p "enter new warning threshold (def 75): " warning
+	read -p "enter new failure threshold (edf 50): " failure
+	sed -i "s/\"warning\": 75/\"warning\": $warning/" attendance_tracker_$input/Helpers/config.json
+	sed -i "s/\"failure\": 50/\"failure\": $failure/" attendance_tracker_$input/Helpers/config.json
+fi
 
