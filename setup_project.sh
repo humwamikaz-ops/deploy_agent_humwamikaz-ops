020d3f437507021761d59e803e070b861cde29ac
humwamikaz-ops <h.umwamikaz@alustudent.com>
2026-06-14 10:58:42 -0700

    Added file handling in case file already exists

--- setup_project.sh
+++ setup_project.sh
@@ -2,11 +2,12 @@
 
 echo "Enter a single name or number:" 
 read input 
-mkdir attendance_tracker_$input 
-mkdir attendance_tracker_$input/Helpers
+mkdir -p attendance_tracker_$input 
+touch attendance_tracker_$input/attendance_checker.py
+mkdir -p attendance_tracker_$input/Helpers
 touch attendance_tracker_$input/Helpers/assets.csv
 touch attendance_tracker_$input/Helpers/config.json
-mkdir attendance_tracker_$input/reports
+mkdir -p attendance_tracker_$input/reports
 touch attendance_tracker_$input/reports/reports.log
 
 
